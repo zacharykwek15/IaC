e258af47dfc19e696db4e5048e3c570f88385991
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-26 10:45:32 +0800

    .

--- main.tf
+++ main.tf
@@ -84,45 +84,6 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-# Creating a IAM role for github yaml file for credential purpose
-resource "aws_iam_role" "github_oidc_deploy" {
-  name = "github-oidc-deploy-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::255945442255:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:zacharykwek15/IaC:ref:refs/heads/main"
-          }
-        }
-        }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "github_oidc_policy" {
-  role       = aws_iam_role.github_oidc_deploy.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "github_oidc_ecr" {
-  role       = aws_iam_role.github_oidc_deploy.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "github_oidc_logs" {
-  role       = aws_iam_role.github_oidc_deploy.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-}
-
 
 
 
